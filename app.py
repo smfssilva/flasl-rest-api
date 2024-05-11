bd6b9acfa31a74531cd57cfe655b10469057dff0
smfssilva <smfssilva@yahoo.com>
2024-05-11 15:34:52 -0300

    First REST-API endpoint

--- app.py
+++ app.py
@@ -2,5 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.route("/")
+
+stores = {
+   "name": "My Store",
+   "items": [
+      {
+         "name": "Chair",
+         "price": 15.99
+      }
+   ]
+}
+
+
+@app.get("/store")
+def get_stores():
+   return {"stores": stores}
 
